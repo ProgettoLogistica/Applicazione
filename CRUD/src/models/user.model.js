466f04ddda35a53e811ff1022b3b6066f9e5b390
veronica-aimar <aimarveronica1@gmail.com>
2021-02-25 09:59:27 +0100

    Ulteriore revisione di user e aggiunta di credential

--- CRUD/src/models/user.model.js
+++ CRUD/src/models/user.model.js
@@ -1,25 +1,23 @@
 'use strict';
 var conn = require('../../config/db.config');
 
-var User = function(user){
+var User = function (user) {
     this.ID = user.ID;
     this.last_name = user.last_name;
     this.first_name = user.first_name;
     this.address = user.address;
     this.phone = user.phone;
     this.email = user.email;
-    this.userName = user.userName;
-    this.password = user.password;
     this.role = user.role;
 };
 
 User.create = function (newUser, result) {
     conn.query("INSERT INTO Users set ?", newUser, function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
         }
-        else{
+        else {
             console.log(res.insertId);
             result(null, res.insertId);
         }
@@ -28,11 +26,11 @@ User.create = function (newUser, result) {
 
 User.findById = function (id, result) {
     conn.query("Select * from Users where ID = ? ", id, function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
         }
-        else{
+        else {
             result(null, res);
         }
     });
@@ -40,37 +38,37 @@ User.findById = function (id, result) {
 
 User.findAll = function (result) {
     conn.query("Select * from Users", function (err, res) {
-        if(err) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
-        else{
+        else {
             console.log('Users : ', res);
             result(null, res);
         }
     });
 };
 
-User.update = function(id, user, result){
-    conn.query("UPDATE Users SET last_name=?, first_name=?, address=?, phone=?, email=?, userName=?, password=?, role=? WHERE id = ?", [user.last_name,user.first_name,user.address,user.phone,user.email,user.userName,user.password,user.role, id], function (err, res) {
-    if(err) {
-        console.log("error: ", err);
-        result(null, err);
-    }else{
-        result(null, res);
-    }
+User.update = function (id, user, result) {
+    conn.query("UPDATE Users SET last_name=?, first_name=?, address=?, phone=?, email=?, role=? WHERE id = ?", [user.last_name, user.first_name, user.address, user.phone, user.email, user.role, id], function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        } else {
+            result(null, res);
+        }
     });
 };
 
-User.delete = function(id, result){
+User.delete = function (id, result) {
     conn.query("DELETE FROM Users WHERE id = ?", [id], function (err, res) {
-    if(err) {
-        console.log("error: ", err);
-        result(null, err);
-    }
-    else{
-        result(null, res);
-    }
+        if (err) {
+            console.log("error: ", err);
+            result(null, err);
+        }
+        else {
+            result(null, res);
+        }
     });
 };
 

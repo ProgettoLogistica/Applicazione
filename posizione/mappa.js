27cb185f62ec508e55b951af666363f53bbc49d9
barileanna <57175052+barileanna@users.noreply.github.com>
2021-03-03 18:46:36 +0100

    Add files via upload

--- posizione/mappa.js
+++ posizione/mappa.js
@@ -37,8 +37,31 @@
   // attesa inserimento indirizzo
   geocoder.on('addresschosen', (evt) => {
     window.setTimeout(() => {
+      coord = evt.coordinate;
       //popup.show(evt.coordinate, evt.address.formatted);
       console.log(evt.address);
+      console.log(evt.coordinate);
+      //dati coordinate indrizzi --> restituisce un json con i dati
+      //implementare routing con leaflet
+      let request = new XMLHttpRequest();
+
+      request.open('POST', "https://api.openrouteservice.org/v2/directions/driving-car");
+
+      request.setRequestHeader('Accept', 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8');
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.setRequestHeader('Authorization', '5b3ce3597851110001cf62487875a4d7868a453ca9b39fe4e41e1862');
+
+      request.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          console.log('Status:', this.status);
+          console.log('Headers:', this.getAllResponseHeaders());
+          console.log('Body:', this.responseText);
+        }
+      };
+
+      const body = '{"coordinates":[[8.681495,49.41461],[8.686507,49.41943],[8.687872,49.420318]]}';
+
+      request.send(body);
     }, 3000);
   });
 })(window, document);

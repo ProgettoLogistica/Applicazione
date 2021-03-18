99c88ea04da85fcd5b6e8557460e56977879c0a9
AreccoAlessiaCN <57175009+AreccoAlessiaCN@users.noreply.github.com>
2021-03-18 10:37:56 +0100

    Update gestionePesoVolume.js

--- gestionePesoVolume/gestionePesoVolume.js
+++ gestionePesoVolume/gestionePesoVolume.js
@@ -2,16 +2,27 @@
 const maxWeight = 1440; //kg
 const maxVolume = 5,43; //m^3
 
-//raccolgo i dati dal database:
-// scorro il database e controllo se ci sono pacchi a priorità 1
-// prendo quelli con priorità alta, calcolo il volume di ogni pacchetto, visualizzo 
-//  il peso, controllo che ci stia e lo "carico" sul furgone
-// // scorro il database e controllo se ci sono pacchi che sono arriati da più di 10 giorni,
-// se ci sono, calcolo il volume di ogni pacchetto, visualizzo 
-//  il peso, controllo che ci stia e lo "carico" sul furgone
-// scorro il database e controllo se ci sono pacchi che hanno più di un tentativo di consegna
-// prendo quelli che hanno già 1 o 2 tentativi, calcolo il volume di ogni pacchetto, visualizzo 
-//  il peso, controllo che ci stia e lo "carico" sul furgone
-// se ho ancora spazio scorro il database e controllo se ci sono pacchi che sono arrivati da più di 3 giorni                                                                                                                                                                                                                                                                                                                                                                                                                                              ,
-// se ci sono, calcolo il volume di ogni pacchetto, visualizzo 
-//  il peso, controllo che ci stia e lo "carico" sul furgone
+//vettore dei pacchetti caricati
+var parcelsUpload = new Array();
+//numero dei pacchetti caricati
+var parcelUpload = 0;
+
+//questo valore e il vettore me lo passerà un'altra funzione
+var maxParcels = 100; 
+var parcels = new Array(parcelId1, parcelId2, parcelId3);
+
+function upload(){
+    for(int i = 0; i < maxParcels && volume < maxVolume && weight < maxWeight; i++){
+    weight = weight + parcels[i].weight;
+    volume = volume + (parcels[i].height * parcels[i].width * parcels[i].depth);
+    parcelsUpload.push(parcels[i]);
+    parcelUpload++;
+    }
+
+    //se il volume o il peso hanno superato il massimo, tolgo l'ultimo pacchetto inseritos
+    if(volume > maxVolume || weight > maxWeight){
+    parcelsUpload.splice(parcelUpload, 1);
+    weight = weight - parcels[i].weight;
+    volume = volume - (parcels[i].height * parcels[i].width * parcels[i].depth);
+    }
+}

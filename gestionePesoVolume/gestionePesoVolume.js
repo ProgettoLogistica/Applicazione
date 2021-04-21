667defd89bbbcec1a6df02d0f998ce08a62b8950
AreccoAlessiaCN <57175009+AreccoAlessiaCN@users.noreply.github.com>
2021-04-21 13:51:06 +0200

    gestionePesoVolume ufficiale
    Penso sia la versione corretta

--- gestionePesoVolume/gestionePesoVolume.js
+++ gestionePesoVolume/gestionePesoVolume.js
@@ -1,6 +1,6 @@
 //capienza massima di volume e peso di un furgoncino standard
 const maxWeight = 1440; //kg
-const maxVolume = 5,43; //m^3
+const maxVolume = 5.43; //m^3
 
 //vettore dei pacchetti caricati
 var parcelsUpload = new Array();
@@ -9,20 +9,20 @@ var parcelUpload = 0;
 
 //questo valore e il vettore me lo passerà un'altra funzione
 var maxParcels = 100; 
-var parcels = new Array(parcelId1, parcelId2, parcelId3);
+<% parcels[0]%>
 
 function upload(){
     for(int i = 0; i < maxParcels && volume < maxVolume && weight < maxWeight; i++){
-    weight = weight + parcels[i].weight;
-    volume = volume + (parcels[i].height * parcels[i].width * parcels[i].depth);
-    parcelsUpload.push(parcels[i]);
+    weight = weight + <% parcels[i].weight%>;
+    volume = volume + (<% parcels[i].height%> * <% parcels[i].width%> * <% parcels[i].depth%>);
+    parcelsUpload.push(<% parcels[i]%>);
     parcelUpload++;
     }
 
     //se il volume o il peso hanno superato il massimo, tolgo l'ultimo pacchetto inseritos
     if(volume > maxVolume || weight > maxWeight){
     parcelsUpload.splice(parcelUpload, 1);
-    weight = weight - parcels[i].weight;
-    volume = volume - (parcels[i].height * parcels[i].width * parcels[i].depth);
+    weight = weight + <% parcels[i].weight%>;
+    volume = volume + (<% parcels[i].height%> * <% parcels[i].width%> * <% parcels[i].depth%>);
     }
 }
